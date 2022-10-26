823df88652a5b8c4eebe6b30e3f857ab391cab94
Andrea Bozzelli <74464364+bozzelliandrea@users.noreply.github.com>
2022-10-26 13:11:42 +0200

    Update basics.js

--- basics.js
+++ basics.js
@@ -1,4 +1,4 @@
-// Basics of JS - 8/7 kyu
+// Basic FOUNDAMENTALS of JS - 8/7 kyu
 //convert a Number to String
 
 const numberToString = (num) => num + ""
@@ -56,6 +56,36 @@ describe("Basic tests", () => {
 function disemvowel(str) {
   return str.replace(/[aAeEiIoOuU]/g, '');
 }
+
+/* array diff
+Your goal in this kata is to implement a difference function, which subtracts one list from another and returns the result.
+
+It should remove all values from list a, which are present in list b keeping their order.
+
+arrayDiff([1,2],[1]) == [2]
+If a value is present in b, all of its occurrences must be removed from the other:
+
+arrayDiff([1,2,2,2,3],[2]) == [1,3]
+*/
+function arrayDiff(a, b) {
+  return a.filter(n => !b.includes(n));  
+}
+
+// tests
+const chai = require("chai");
+const assert = chai.assert;
+chai.config.truncateThreshold=0;
+
+describe("Basic tests", () => {
+  it("Should pass Basic tests", () => {
+    assert.deepEqual(arrayDiff([1,2], [1]), [2], "a was [1,2], b was [1]");
+    assert.deepEqual(arrayDiff([1,2,2], [1]), [2,2], "a was [1,2,2], b was [1]");
+    assert.deepEqual(arrayDiff([1,2,2], [2]), [1], "a was [1,2,2], b was [2]");
+    assert.deepEqual(arrayDiff([1,2,2], []), [1,2,2], "a was [1,2,2], b was []");
+    assert.deepEqual(arrayDiff([], [1,2]), [], "a was [], b was [1,2]");
+    assert.deepEqual(arrayDiff([1,2,3], [1,2]), [3], "a was [1,2,3], b was [1,2]")
+  });
+});
 
 
 
